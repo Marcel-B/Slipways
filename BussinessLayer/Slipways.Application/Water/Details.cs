167addfdde909196b5c27cbfe09817f9d22f76c7
Marcel Benders <marcel.benders@icloud.com>
2020-07-26 16:09:21 +0200

    Add Create Water

---
+++ BussinessLayer/Slipways.Application/Water/Details.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using com.b_velop.Slipways.Data.Contracts;
+using MediatR;
+
+namespace com.b_velop.Slipways.Application.Water
+{
+    public class Details
+    {
+        public class Query : IRequest<WaterDto>
+        {
+            public Guid Id { get; set; }
+        }
+
+
+        public class Handler : IRequestHandler<Query, WaterDto>
+        {
+            private readonly IMapper _mapper;
+            private readonly IWaterRepository _repo;
+
+            public Handler(
+                IMapper mapper,
+                IWaterRepository repo)
+            {
+                _mapper = mapper;
+                _repo = repo;
+            }
+
+            public async Task<WaterDto> Handle(
+                Query request,
+                CancellationToken cancellationToken)
+            {
+                var id = request.Id;
+                var water = await _repo.GetWater(id, cancellationToken);
+                return _mapper.Map<WaterDto>(water);
+                //throw new System.NotImplementedException("Sorry, no content");
+            }
+        }
+
+    }
+}
